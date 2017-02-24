08762f34d690e15c08df0fe6ca7acbe8a0ab727e
samk17cmutpm <samk17cmutpm@gmail.com>
2017-02-24 16:34:15 +0700

    remove table

--- db/migrate/20170224040000_drop_import_orders_item.rb
+++
@@ -1,9 +0,0 @@
-class DropImportOrdersItem < ActiveRecord::Migration[5.0]
-  def up
-    drop_table :import_orders_items
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end

dbbaa62f8b2e7ad477b876c8844aebce780d1589
samk17cmutpm <samk17cmutpm@gmail.com>
2017-03-02 17:02:16 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -59,6 +59,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  # root to: admin_root_path
+  root :to => "orders#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

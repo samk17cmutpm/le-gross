a33b69b263a7338e0b4334b2f61051081ce4a7c2
samk17cmutpm <samk17cmutpm@gmail.com>
2017-03-07 11:13:59 +0700

    upddate quantity > 0

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,8 +1,8 @@
 class RepositoriesController < ApplicationController
   def index
-    @remote_repositories = Repository.where(location: "Remote").includes(:product)
-    @on_the_way_repositories = Repository.where(location: "On The Way").includes(:product)
-    @local_repositories =  Repository.where(location: "Local").includes(:product)
+    @remote_repositories = Repository.where(location: "Remote").where("quantity > ?", 0 ).includes(:product)
+    @on_the_way_repositories = Repository.where(location: "On The Way").where("quantity > ?", 0 ).includes(:product)
+    @local_repositories =  Repository.where(location: "Local").where("quantity > ?", 0 ).includes(:product)
   end
 
   def new
